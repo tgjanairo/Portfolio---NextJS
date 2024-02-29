196a8ab763fc9df69a70c3d6dc346f5c089235e5
Terrence Janairo <tgjanairo@pldt.com.ph>
2024-02-29 23:05:23 +0800

    Edited Font

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -9,8 +9,8 @@ const HeroSection = () => {
   return (
     <section className="lg:py-16">
       <div className="grid grid-cols-1 sm:grid-cols-12">
-        <div className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+        <div className="col-span-7 place-self-center text-center sm:text-left justify-self-start">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
               Hello, I&apos;m{" "}
             </span>
