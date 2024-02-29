9721bf32772e901758d19e696e3e7b223b8f86f9
Terrence Janairo <tgjanairo@pldt.com.ph>
2024-02-29 23:41:04 +0800

    Edited Fonts

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,28 +1,25 @@
-import { NextResponse } from "next/server";
-import { Resend } from "resend";
+// import { EmailTemplate } from '../../../components/EmailTemplate';
+import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(req, res) {
-  const { email, subject, message } = await req.json();
-  console.log(email, subject, message);
+export async function POST() {
   try {
     const data = await resend.emails.send({
-      from: fromEmail,
-      to: [fromEmail, email],
-      subject: subject,
+      from: 'Terrence <janairoterrence@gmail.com>',
+      to: ['janairoterrence@gmail.com'],
+      subject: 'Hello world',
       react: (
         <>
-          <h1>{subject}</h1>
-          <p>Thank you for contacting us!</p>
-          <p>New message submitted:</p>
-          <p>{message}</p>
+        <p>
+          Email Body
+        </p>
         </>
       ),
     });
-    return NextResponse.json(data);
+
+    return Response.json(data);
   } catch (error) {
-    return NextResponse.json({ error });
+    return Response.json({ error });
   }
 }
